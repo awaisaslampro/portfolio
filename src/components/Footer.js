f7cd3a6e966c5458db33894497c11c88c6805cf8
awaisaslampro <awaisaslampro@gmail.com>
2023-01-11 00:38:33 +0000

    Footer started

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub, FaMailBulk } from "react-icons/fa";
 function Footer() {
     return (
         <div>
+            <div>
+                <div className="bg-theme p-10 text-center">
+                    <p className="text-gray-50">Design and Develope by</p>
+                    <div className="flex">
+                        <FaLinkedin />
+                        <FaFacebook />
+                        <FaInstagram />
+                        <FaMailBulk />
+                        <FaLinkedin />
+                        <FaGithub />
 
-            Footer
+                    </div>
+                    <p className="text-gray-50">Muhammad Awais</p>
+                </div>
+            </div>
         </div>
     )
 }
