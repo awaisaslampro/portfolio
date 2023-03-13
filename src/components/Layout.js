edf32c06c2b81c4f55b78b6bc5871b7bd42a9b1f
MuhammadBao <acfnu720@gmail.com>
2023-03-13 17:08:45 +0100

    Footer changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,12 @@
-import { React, useState } from "react";
+import React from "react";
 import Footer from './Footer';
 import Header from './Header';
 const Layout = ({ children }) => {
-    const [say, setSay] = useState('Good Morning');
-
-    const handleSay = () => {
-        setSay("Boa Noite")
-    }
-
     return (
         <div>
             <Header />
             <div className="content">{children}</div>
-
-            <div>Say something: {say}</div>
-            <Footer changeSay={handleSay} name="Awais" girlFriend="Stephnie" />
+            <Footer />
         </div>
     )
 }
